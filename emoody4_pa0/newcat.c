f96d8a02d28dcabdb7e937d576ac47c9e707150d
TheThingKnownAsKit <moodyellam@gmail.com>
2026-09-01 12:27:29 -0500

    finished newcat

--- emoody4_pa0/newcat.c
+++ emoody4_pa0/newcat.c
@@ -1,4 +1,22 @@
+#include <stdio.h>
+
 int main(int argc, char *argv[])
 {
-    return 0;
+    for (int i = 1; i < argc; i++)
+    {
+        FILE *file = fopen(argv[i], "r");
+        if (file == NULL)
+        {
+            perror("Cannot open file");
+            return 1;
+        }
+
+        char content;
+        while ((content = fgetc(file)) != EOF)
+        {
+            putchar(content);
+        }
+
+        fclose(file);
+    }
 }
